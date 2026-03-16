062ee2ce6de4d95b9a28b8e4cb14369468866db3
Sato <sato@neoproxy.art>
2026-03-16 03:03:26 -0300

    chore: remove tracked artifacts and update gitignore for gennos repo

--- artifacts/neoproxy/.next/server/app/api/catalog/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/catalog/route.js")
-R.c("server/chunks/[root-of-the-server]__78d471cf._.js")
-R.c("server/chunks/[root-of-the-server]__f408c708._.js")
-R.c("server/chunks/artifacts_neoproxy__next-internal_server_app_api_catalog_route_actions_6b26f73f.js")
-R.m(58288)
-module.exports=R.m(58288).exports
--- artifacts/neoproxy/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CMB/mLVhPEf1aZ0Ou2pkDCsFf3w1KJUYArchOWmlYvk=\"\n}"
